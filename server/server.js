0e7374a7cd0c1ac133388be757b7d55bcd7e6b4f
Janidu <96234491+JaniduR1@users.noreply.github.com>
2023-10-19 01:28:54 +0000

    Almost working POST, need to modify image field and add dateTo field

--- server/server.js
+++ server/server.js
@@ -11,8 +11,31 @@ app.get('/', (req, res) => {
 app.post('/item', (req, res) => {
   console.log("POST to item")
   //console.log(req.body)
-  //var data = req.body
-  //console.log(data)
+  const dateFrom = new Date().toISOString(); //Current Date From to ISO Format 
+  const id = Math.random() //Generate Random ID
+  const data = req.body
+  //data.date_to = dateTo;
+
+  data.id = id; //Adds ID field to body and sets it to const id above
+  data['Date From'] = dateFrom; //Creates Date From field
+
+  //data["date_from:" ] = dateFrom // Adds Date_From field to body
+  //data["id:" ] = id // Adds ID field to body
+
+  // Format Body Field Order
+  const orderdFields = {
+    "id": data.id,
+    "user_id": data.user_id,
+    "keywords": data.keywords,
+    "description": data.description,
+    "image": data.image,
+    "lat": data.lat,
+    "lon": data.lon,
+    "date_from": data['Date From'],
+  };
+
+
+  console.log(orderdFields)
   res.status(201).json()
 })
 
