c6e14c363016396d4414c5c7ccb6ae62d6d202b2
Janidu <96234491+JaniduR1@users.noreply.github.com>
2023-10-23 02:35:05 +0000

    Server Almost Complete -- Need Validation on Delete end point Need to add Date_To to POST

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const ITEMS = 
+let ITEMS = 
 [
   {
     "id": 0,
@@ -27,60 +27,84 @@ const ITEMS =
   }
 ]
 
+// POST
 
 app.post('/item', (req, res) => {
-  console.log("POST to item")
+  //console.log("POST to item")
   //console.log(req.body)
   const dateFrom = new Date().toISOString(); //Current Date From to ISO Format 
   const id = Math.random() //Generate Random ID
   const data = req.body
   //data.date_to = dateTo;
 
-  data.id = id; //Adds ID field to body and sets it to const id above
-  data['Date From'] = dateFrom; //Creates Date From field
 
-  //data["date_from:" ] = dateFrom // Adds Date_From field to body
-  //data["id:" ] = id // Adds ID field to body
-
-  // Format Body Field Order
-  const orderdFields = {
-    "id": data.id,
-    "user_id": data.user_id,
-    "keywords": data.keywords,
-    "description": data.description,
-    "image": data.image,
-    "lat": data.lat,
-    "lon": data.lon,
-    "date_from": data['Date From'],
-  };
+  // Validation
+  if (!data.user_id || !data.keywords || !data.description || !data.lat || !data.lon)
+  {
+    console.log("Error 405 - Invalid input - Some input fields are missing.")
+    res.status(405).json({ error: "Invalid input - Some input fields are missing." });
+  }
+  else
+  {
 
 
-  console.log(orderdFields)
-  ITEMS.push(orderdFields)
-  res.status(201).json()
+    data.id = id; //Adds ID field to body and sets it to const id above
+    data['Date From'] = dateFrom; //Creates Date From field
+
+    //data["date_from:" ] = dateFrom // Adds Date_From field to body
+    //data["id:" ] = id // Adds ID field to body
+
+    // Format Body Field Order
+    const orderdFields = 
+    {
+      "id": data.id,
+      "user_id": data.user_id,
+      "keywords": data.keywords,
+      "description": data.description,
+      "image": data.image,
+      "lat": data.lat,
+      "lon": data.lon,
+      "date_from": data['Date From'],
+    };
+
+
+    console.log(orderdFields)
+    ITEMS.push(orderdFields)
+    res.status(201).json()
+    console.log("Item created successfully")
+  }
 })
 
-
+// Get Items end point
 app.get('/items', (req, res) => {
   res.json(ITEMS)
 })
 
+
+// Get single item by ID
 app.get('/item/:id', (req, res) => {
-  console.log("I will get this in the future " + req.params.id)
+  //console.log("I will get this in the future " + req.params.id)
   for (let item of ITEMS)
   {
     //console.log(i)
     if (item.id == req.params.id)
     {
-      console.log("Item Found!")
+      console.log("Successful Operation - Item Found!")
       console.log(item)
-      res.status(200)
+      res.status(200).json()
       return;
     }
   }
-
+  console.log("Error 404 - Item not found")
   res.status(404).json({ error: "Item not found" });
+})
 
+
+// Delete item by ID
+app.delete('/item/:id', (req, res) => {
+  //console.log("I will delete this in the future " + req.params.id)
+  ITEMS = ITEMS.filter((item)=> item.id != req.params.id)
+  res.status(204).json()
 })
 
 app.listen(port, () => {
