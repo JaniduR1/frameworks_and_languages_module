553889f098d97bbe935f85153897a553e7a8baaf
Janidu <96234491+JaniduR1@users.noreply.github.com>
2023-11-10 22:40:19 +0000

    Added Validation for POST && /items/ end point working

--- falcon_server/server.py
+++ falcon_server/server.py
@@ -5,47 +5,69 @@ import random
 from wsgiref.simple_server import make_server
 
 
-class GetItems:
-    def on_get(self, req, resp, **kwargs):
-        resp.text = ('POST Works')
 
-        message = req.get_media()
+ITEMS = [
+      {
+        "id": 0,
+        "user_id": "user1234",
+        "keywords": 
+        [
+        "hammer",
+        "nails",
+        "tools"
+        ],
+        "description": "A hammer and nails set",
+        "image": "https://placekitten.com/200/300",
+        "lat": 51.2798438,
+        "lon": 1.0830275,
+        "date_from": "2023-10-20T22:57:05.814Z",
+        "date_to": "2023-10-20T22:57:05.814Z"
+  }
+]
 
-        resp.media = {'message': message}
-        resp.status = falcon.HTTP_200
-
+# Post an Item to ITEMS list
 class PostItem:
     def on_post(self, req, resp):
 
         data = json.loads(req.bounded_stream.read().decode('utf-8'))
 
-        date_from = datetime.datetime.utcnow().isoformat()
-        date_to = datetime.datetime.utcnow().isoformat()
-        item_id = random.randint(1,100000)
+        if data.get("user_id") is None or data.get("keywords") is None or data.get("description") is None or data.get("lat") is None or data.get("lon") is None:
+            resp.media = {'Error 405': 'Invalid input - Some input fields are missing.'}
+            resp.status = falcon.HTTP_405
+        else:
+            date_from = datetime.datetime.utcnow().isoformat()
+            date_to = datetime.datetime.utcnow().isoformat()
+            item_id = random.randint(1,100000)
 
-        data['id'] = item_id
-        data['Date From'] = date_from
-        data['Date To'] = date_to
+            data['id'] = item_id
+            data['Date From'] = date_from
+            data['Date To'] = date_to
 
-        ordered_fields = {
-            "id": data.get('id'),
-            "user_id": data.get('user_id'),
-            "keywords": data.get('keywords'),
-            "description": data.get('description'),
-            "image": data.get('image'),
-            "lat": data.get('lat'),
-            "lon": data.get('lon'),
-            "date_from": data.get('Date From'),
-            "date_to": data.get('Date To'),
-        }
+            ordered_fields = {
+                "id": data.get('id'),
+                "user_id": data.get('user_id'),
+                "keywords": data.get('keywords'),
+                "description": data.get('description'),
+                "image": data.get('image'),
+                "lat": data.get('lat'),
+                "lon": data.get('lon'),
+                "date_from": data.get('Date From'),
+                "date_to": data.get('Date To'),
+            }
 
-        print(ordered_fields)
+            print(ordered_fields)
+            ITEMS.append(ordered_fields)
+            resp.status = falcon.HTTP_201
 
-        resp.status = falcon.HTTP_201
 
+# Getting All Items
+class GetItems: 
+    def on_get(self, req, resp, **kwargs):
+
+        resp.media = ITEMS
+        resp.status = falcon.HTTP_200
 
 app = falcon.App()
-
 
 app.add_route('/items', GetItems())
 app.add_route('/item', PostItem())
